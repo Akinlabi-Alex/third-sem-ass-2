e95f41c7cd932bda448d1e95f2c611ed138f1eec
Akinlabi Alexander <148863693+Akinlabi-Alex@users.noreply.github.com>
2024-06-29 08:23:01 +0100

    variable.tf

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "aws-region" {
+    default = "us-east-1"
+}
+variable "domain-name" {
+    default = "var_domain.com"
+}
+variable "subdomain-name" {
+    default = "www"
+}
+variable "env" {
+    default = "dev"
+}
+variable "s3-bucket-name" {
+      default  ="static-bucket"
+}
